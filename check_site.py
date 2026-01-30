f16d89d320b8c9e0f684407efee3e6b64e94e8af
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2026-01-30 09:57:45 +0100

    Create check_site.py

---
+++ check_site.py
@@ -0,0 +1,16 @@
+import requests
+
+URL = "https://integrationsmonitoring.niedersachsen.de/"
+
+try:
+    response = requests.get(URL, timeout=20)
+
+    if response.status_code != 200:
+        print(f"Fehler: HTTP {response.status_code}")
+        exit(1)
+
+    print("Seite ist erreichbar.")
+
+except Exception as e:
+    print(f"Exception: {e}")
+    exit(1)
